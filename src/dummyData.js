37c5444257ef8d286942cb25cb6602d19ae1a7d7
FarelFT <farelft02@gmail.com>
2021-11-29 21:54:30 +0700

    feat: admin

---
+++ src/dummyData.js
@@ -0,0 +1,54 @@
+export const userData = [
+  {
+    name: 'Jan',
+    'Active User': 4000,
+  },
+  {
+    name: 'Feb',
+    'Active User': 3000,
+  },
+  {
+    name: 'Mar',
+    'Active User': 4000,
+  },
+  {
+    name: 'Apr',
+    'Active User': 4000,
+  },
+  {
+    name: 'May',
+    'Active User': 4000,
+  },
+  {
+    name: 'Jun',
+    'Active User': 2000,
+  },
+  {
+    name: 'Jul',
+    'Active User': 1000,
+  },
+  {
+    name: 'Agu',
+    'Active User': 2000,
+  },
+  {
+    name: 'Jan',
+    'Active User': 3000,
+  },
+  {
+    name: 'Sep',
+    'Active User': 3500,
+  },
+  {
+    name: 'Oct',
+    'Active User': 4000,
+  },
+  {
+    name: 'Nov',
+    'Active User': 4000,
+  },
+  {
+    name: 'Des',
+    'Active User': 5000,
+  },
+];
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,20 @@
+import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+import Chart from '../../components/chart/Chart';
+import './Home.css';
+import { userData } from '../../dummyData';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <FeaturedInfo />
+      <Chart
+        data={userData}
+        title='User Analytics'
+        grid
+        dataKey='Active User'
+      />
+    </div>
+  );
+};
+
+export default Home;
