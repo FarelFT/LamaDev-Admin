b9dd524d20824340fa294feac9ef31fc5b4d1e1d
FarelFT <farelft02@gmail.com>
2021-12-01 19:50:28 +0700

    feat: complete admin

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
-import './App.css';
 import Home from './pages/home/Home';
+import UserList from './pages/userList/UserList';
+import User from './pages/user/User';
+import NewUser from './pages/newUser/NewUser';
+import ProductList from './pages/productList/ProductList';
+import Product from './pages/product/Product.jsx';
+import NewProduct from './pages/newProduct/NewProduct';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Topbar />
       <div className='container'>
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/users' element={<UserList />} />
+          <Route path='/user/:userId' element={<User />} />
+          <Route path='/newUser' element={<NewUser />} />
+          <Route path='/products' element={<ProductList />} />
+          <Route path='/product/:productId' element={<Product />} />
+          <Route path='/newproduct' element={<NewProduct />} />
+        </Routes>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
